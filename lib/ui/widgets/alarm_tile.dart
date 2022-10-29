38d96744b783bdc079f98972905a31f7443b9968
Sarah <sarah.jessica.scholz@mni.thm.de>
2022-10-29 11:16:02 +0200

    added function _pushChangeAlarmScreen() / added variable isDeleted and return statement if isDeleted is true

--- lib/ui/widgets/alarm_tile.dart
+++ lib/ui/widgets/alarm_tile.dart
@@ -10,13 +10,15 @@ class AlarmTile extends StatefulWidget {
   String name;
   bool isOn;
   bool rep;
+  bool isDeleted = false;
 
   AlarmTile({Key? key,
     required this.index,
     required this.time,
     required this.name,
     required this.isOn,
-    required this.rep }) : super(key: key);
+    required this.rep,
+  }) : super(key: key);
 
 
   @override
@@ -25,21 +27,26 @@ class AlarmTile extends StatefulWidget {
 
 class _AlarmTileState extends State<AlarmTile> {
 
-
   @override
   Widget build(BuildContext context) {
 
-    String hour = widget.time.hour < 10 ? '0${widget.time.hour}' : widget.time.hour.toString();
-    String minute = widget.time.minute < 10 ? '0${widget.time.minute}' : widget.time.minute.toString();
+    if (widget.isDeleted) return const SizedBox(); // gibt es eine bessere Lösung??
+
+    String hour = widget.time.hour < 10 ? '0${widget.time.hour}' : widget.time
+        .hour.toString();
+    String minute = widget.time.minute < 10 ? '0${widget.time.minute}' : widget
+        .time.minute.toString();
 
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
       child: Card(
         margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
         child: ListTile(
-          leading: Text('$hour : $minute', style: const TextStyle(fontSize: 25.0),),
+          leading: Text(
+            '$hour : $minute', style: const TextStyle(fontSize: 25.0),),
           title: Text(widget.name, style: const TextStyle(fontSize: 20.0),),
-          subtitle: Text(widget.rep ? 'Daily' : 'Once', style: const TextStyle(fontSize: 20.0), ),
+          subtitle: Text(widget.rep ? 'Daily' : 'Once',
+            style: const TextStyle(fontSize: 20.0),),
           trailing: Switch(
             value: widget.isOn,
             onChanged: (val) {
@@ -50,9 +57,32 @@ class _AlarmTileState extends State<AlarmTile> {
             },
             activeColor: Colors.black,
           ),
-          onTap: () {Navigator.pushReplacementNamed(context, '/change_alarm', arguments: {'index' : widget.index});},
+          onTap: () {
+            _pushChangeAlarmScreen(context);
+          },
         ),
       ),
     );
+  }
+
+  Future<void> _pushChangeAlarmScreen(BuildContext context) async {
+    final result = await Navigator.pushNamed(
+        context,
+        '/change_alarm',
+        arguments: {'index': widget.index}
+    );
+    if (result == 'Yes') {
+      setState(() {
+        widget.name = alarms[widget.index].name;
+        widget.isOn = alarms[widget.index].isOn;
+        widget.time = alarms[widget.index].time;
+        widget.time = alarms[widget.index].time;
+      });
+    }
+    if (result == 'Deleted') {
+      setState(() {
+        widget.isDeleted = true;
+      });
+    }
   }
 }
