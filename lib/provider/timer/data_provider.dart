cbcfecb002b021abfbfe607cc5e521dc51be1eea
Sarah <sarah.jessica.scholz@mni.thm.de>
2022-11-02 09:55:58 +0100

    added an instance of the StopWatchTimer; added a variable; added a function to reset the stopWatchTimer

--- lib/provider/timer/data_provider.dart
+++ lib/provider/timer/data_provider.dart
@@ -1,4 +1,20 @@
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+import 'package:uhr/services/notification_service.dart';
 
 // Klasse mit Informationen auf die andere Dateien Zugriff haben sollen
 
 DateTime setTime = DateTime(0, 0, 0, 0, 0, 0);
+
+StopWatchTimer stopWatchTimer = StopWatchTimer(
+    mode: StopWatchMode.countDown,
+    onEnded: () => NotificationService().timerScreenNotification(999, 'Timer expired', ''),
+);
+
+bool isPaused = false;
+
+void resetStopWatchTimer () {
+  stopWatchTimer = StopWatchTimer(
+    mode: StopWatchMode.countDown,
+    onEnded: () => NotificationService().timerScreenNotification(999, 'Timer expired', ''),
+  );
+}
