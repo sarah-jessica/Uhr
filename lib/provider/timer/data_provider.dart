3c2a3c014fd26651d5ceebcce42593d3fea65874
Sarah <sarah.jessica.scholz@mni.thm.de>
2022-11-10 18:06:47 +0100

    added Class MyTimer which extends ChangeNotifier

--- lib/provider/timer/data_provider.dart
+++ lib/provider/timer/data_provider.dart
@@ -1,20 +1,54 @@
+import 'package:flutter/material.dart';
 import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'package:uhr/services/notification_service.dart';
+import 'package:provider/provider.dart';
+import 'package:uhr/ui/screens/timer/timer_screen.dart';
 
-// Klasse mit Informationen auf die andere Dateien Zugriff haben sollen
+// Datei mit Informationen, auf die andere Dateien Zugriff haben sollen
 
 DateTime setTime = DateTime(0, 0, 0, 0, 0, 0);
+MyTimer myTimer = MyTimer();
 
-StopWatchTimer stopWatchTimer = StopWatchTimer(
-    mode: StopWatchMode.countDown,
-    onEnded: () => NotificationService().timerScreenNotification(999, 'Timer expired', ''),
-);
+class MyTimer extends ChangeNotifier{
 
-bool isPaused = false;
-
-void resetStopWatchTimer () {
-  stopWatchTimer = StopWatchTimer(
+  StopWatchTimer _stopWatchTimer = StopWatchTimer(
     mode: StopWatchMode.countDown,
     onEnded: () => NotificationService().timerScreenNotification(999, 'Timer expired', ''),
   );
+
+  bool _isPaused = false;
+
+  bool get isRunning => _stopWatchTimer.isRunning;
+  bool get isPaused => _isPaused;
+  dynamic get rawTime => _stopWatchTimer.rawTime;
+  int get rawTimeValue => _stopWatchTimer.rawTime.value;
+
+  void start () {
+    if (_isPaused) {
+      _stopWatchTimer.onStartTimer();
+      _isPaused = false;
+    } else {
+      _stopWatchTimer = StopWatchTimer(
+        mode: StopWatchMode.countDown,
+        onEnded: () => NotificationService().timerScreenNotification(999, 'Timer expired', ''),
+      );
+      _stopWatchTimer.setPresetHoursTime(setTime.hour);
+      _stopWatchTimer.setPresetMinuteTime(setTime.minute);
+      _stopWatchTimer.setPresetSecondTime(setTime.second);
+      _stopWatchTimer.onStartTimer();
+      notifyListeners();
+    }
+  }
+
+  void pause () {
+    _stopWatchTimer.onStopTimer();
+    _isPaused = true;
+  }
+
+  void stop () {
+    _stopWatchTimer.onStopTimer();
+    _isPaused = false;
+    notifyListeners();
+  }
+
 }
